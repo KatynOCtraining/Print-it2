0e916ef9313870cac67356958aae989f786f3a67
KatynOCtraining <t.theveupolaris@gmail.com>
2023-11-18 00:59:46 +0100

    script indentation

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,65 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  
-const slides = [
-	{
-		"image": "slide1.jpg",
-		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image": "slide4.jpg",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-];
 
+    const slides = [
+        {
+            "image": "slide1.jpg",
+            "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+        },
+        {
+            "image": "slide2.jpg",
+            "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+        },
+        {
+            "image": "slide3.jpg",
+            "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+        },
+        {
+            "image": "slide4.jpg",
+            "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+        }
+    ];
+
+    // Déclaration de la variable pour garder une trace du diaporama actuel //
     let currentSlide = 0;
+
+    // Sélection des éléments à afficher dans le DOM //
     const bannerImg = document.querySelector('.banner-img');
     const tagLine = document.querySelector('#banner p');
     const dotsContainer = document.querySelector('.dots');
 
-    // Fonction de mise à jour de l'affichage du carousel //
+    // Fonction pour mettre à jour le carrousel //
     function updateCarousel() {
+        // Affection de l'image et de la tagline en fonction de la slide //
         bannerImg.src = `assets/images/slideshow/${slides[currentSlide].image}`;
         tagLine.innerHTML = slides[currentSlide].tagLine;
+
+        // Mise à jour des bullets points//
         updateDots();
     }
 
-    // Génération et gestion des dots //
+    // Fonction qui crée un bulletpoint à chaque slide //
     function createDots() {
+        // Pour chaque salide, créer un bullet point //
         slides.forEach((_, index) => {
             const dot = document.createElement('span');
             dot.classList.add('dot');
+
+            // EventListener sur les bullets points //
             dot.addEventListener('click', () => {
                 currentSlide = index;
                 updateCarousel();
             });
+
+            // Affichage du bulletpoint //
             dotsContainer.appendChild(dot);
         });
     }
 
+    // Fonction de maj des bullets points//
     function updateDots() {
+        // Parcour chaque point et mets sa classe à jour si son index correspond à celui de la slide en cours//
         document.querySelectorAll('.dots .dot').forEach((dot, index) => {
             if (index === currentSlide) {
                 dot.classList.add('dot_selected');
@@ -56,19 +69,19 @@ const slides = [
         });
     }
 
-    // Naviguer vers la gauche //
+    // Ajout eventlistener sur la flèche de gauche pour aller au diaporama précédent //
     document.querySelector('.arrow_left').addEventListener('click', () => {
         currentSlide = (currentSlide - 1 + slides.length) % slides.length;
         updateCarousel();
     });
 
-    // Naviguer vers la droite //
+    // Ajout d'un eventlistener sur la flèche droite pour aller au diaporama suivant //
     document.querySelector('.arrow_right').addEventListener('click', () => {
         currentSlide = (currentSlide + 1) % slides.length;
         updateCarousel();
     });
 
-    // Initialisation du Carousel // 
+    // Afichage par défaut du Carousel // 
     createDots();
     updateCarousel();
 });
